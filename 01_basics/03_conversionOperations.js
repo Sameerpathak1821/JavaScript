ba88a9d27b938b495085148c43ce93b474dc2e30
Sameer Patahak <sameerpathak2021@gmail.com>
2023-10-26 09:08:51 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,82 @@
+// let score = "33abc";  // not guaranteed ki score is a number 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+ 
+// let valueInNumber = Number(score);
+// console.log(valueInNumber); //Now it is guaranteed that score is a number
+
+
+// console.log(typeof valueInNumber); // the string is converted to number but its type is NaN
+
+// /*
+// 1) string =>NaN
+// 2) Null =>0
+// 3) undefined =>NaN
+// 4) true =>1
+// 5) false =>0
+// 6) "33" =>33
+// 7)"33abc" =>Nan [but the type is still Number]
+
+//  */
+
+
+// let isLoggedIn = 1 ;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+// let temperature = null;
+// let temp = Number(temperature);
+// console.log(temp);
+// console.log(typeof temp);
+
+// /* 
+// ANY DATATYPE TO BOOLEAN
+//    1=>true
+//    0=>false
+//    ""=>false
+//    "sameer"=> true
+
+// */
+
+// let number = 33;
+// let numberToString =String(number);
+// console.log(numberToString);
+// console.log(typeof numberToString);
+
+
+//****************OPERATIONS********************/
+
+let value = 5;
+let negativeValue= -value;
+console.log(negativeValue);
+console.log(2+2); //addition
+console.log(2-2); //substraction
+console.log(2*2); //product
+console.log(2**3); //power [2 to the power 3]
+console.log(2/2); // division
+console.log(2%2); //remainder
+
+// CONCATINATION - Addition of two strings.
+ 
+let str1 ="Sameer";
+let str2 =" pathak";
+let str3 = str1+str2;
+console.log(str3);
+
+/****************************************************/
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); // 32
+
+
+
+
+
+
+
+
