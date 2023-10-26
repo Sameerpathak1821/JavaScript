13c2530aa47b10cae1a9c94246b82808125fd377
Sameer Patahak <sameerpathak2021@gmail.com>
2023-10-26 06:35:54 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version.
+
+/*   
+1) number  =>0 to 2topower 53.
+2) bigint
+3) string  => "string" or 'string'.
+4) boolean => true/false
+5) null  => it is a datatype as well as a standalone value.
+6) undefined => means value abhi assign nhi hui hai
+7) symbol  => unique
+
+8) object
+*/
+
+console.log(typeof "sameer"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
