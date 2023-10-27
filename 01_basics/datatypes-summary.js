2dcaa8445cce4b11936dc08e5224a4556a9934d4
Sameer Patahak <sameerpathak2021@gmail.com>
2023-10-27 10:11:22 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+/*******INTERVIEW PERSPECTIVE********/
+/*
+  Based on how data is store in the memeory and acessed,there are two kind of data types.
+  1)primitive[7 types]:-these are call by value kind of datatype means when we call them the memory will give a copy insted of their memor reference, and if we change them the change happens in their copy.
+    A)string
+    B)number
+    C)boolean
+    D)null
+    E)undefined
+    F)symbol
+    G)bigInt
+
+ javaScript is a dynamically typed langauge.
+
+  2)non-primitive [reference type] :-are the data types whose reference in the memory are allocated if we call them
+
+  A)Arrays
+  B)Objects
+  C)Functions
+
+
+ */
