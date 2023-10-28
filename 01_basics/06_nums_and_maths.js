74e6d487925014d8a9035d20b4cb6e6cc7ca9c1d
Sameer Patahak <sameerpathak2021@gmail.com>
2023-10-28 06:36:05 +0000

    maths and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+// const score = 600
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  //decimal ke baad 2 digit aa jaiyega.
+
+// const randomNum = 10000000
+// console.log(randomNum.toLocaleString('en-IN'));
+
+/**************************************************/
+
+// console.log(Math.abs(-4)); // negative to positive value.
+
+// console.log(Math.round(4.6)); // give round off value
+
+// console.log(Math.random()); // give random number between 0 and 1.
+
+const min = 10;
+const max = 20;
+// important formula to generate a random number between two given numbers.
+console.log(Math.floor ((Math.random() * (max - min +1)+ min)));
